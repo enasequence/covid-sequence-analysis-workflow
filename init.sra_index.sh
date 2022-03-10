1d6599ae6a98f7330651c8e57e6c36bfbd3fa9f5
davidyuyuan <davidyuan@ebi.ac.uk>
2022-03-10 11:14:19 +0000

    Update pipeline versions to V2.

--- init.sra_index.sh
+++ init.sra_index.sh
@@ -14,14 +14,14 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ############################################################
 # Get all raw reads for Nanopore and Illumina into Big Query
 ############################################################
-# It is possible that ILLUMINA raw reads are mislabeled as OXFORD_NANOPORE: grep '_2.fastq.gz' nanopore_index.tsv | wc -l is 296
 curl -X POST -H "Content-Type: application/x-www-form-urlencoded" \
   -d 'result=read_run&query=tax_tree(2697049)%20AND%20(instrument_platform%3D%22ILLUMINA%22)&fields=instrument_platform%2Cinstrument_model%2Cfastq_aspera%2Cfastq_bytes%2Cfastq_ftp%2Cfastq_galaxy%2Cfastq_md5%2Cfirst_created%2Cfirst_public%2Ccountry%2Ccollection_date%2Cisolate%2Cstrain&format=tsv&limit=0' \
   "https://www.ebi.ac.uk/ena/portal/api/search" > "${DIR}/illumina_index.tsv" && \
   gsutil -m cp "${DIR}/illumina_index.tsv" "gs://${dataset_name}/illumina_index.tsv" && \
   bq --project_id="${project_id}" load --source_format=CSV --replace=true --skip_leading_rows=1 --field_delimiter=tab \
-  --autodetect --max_bad_records=5 "${dataset_name}.sra_index" "gs://${dataset_name}/illumina_index.tsv"
+  --autodetect --max_bad_records=0 "${dataset_name}.sra_index" "gs://${dataset_name}/illumina_index.tsv"
 
+# It is possible that ILLUMINA raw reads are mislabeled as OXFORD_NANOPORE: grep '_2.fastq.gz' nanopore_index.tsv | wc -l is 296
 curl -X POST -H "Content-Type: application/x-www-form-urlencoded" \
   -d 'result=read_run&query=tax_tree(2697049)%20AND%20(instrument_platform%3D%22OXFORD_NANOPORE%22)&fields=instrument_platform%2Cinstrument_model%2Cfastq_aspera%2Cfastq_bytes%2Cfastq_ftp%2Cfastq_galaxy%2Cfastq_md5%2Cfirst_created%2Cfirst_public%2Ccountry%2Ccollection_date%2Cisolate%2Cstrain&format=tsv&limit=0' \
   "https://www.ebi.ac.uk/ena/portal/api/search" > "${DIR}/nanopore_index.tsv" && \
