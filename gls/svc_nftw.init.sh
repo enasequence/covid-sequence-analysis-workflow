e3ee48324063740199c01ec08595748daf1faeea
davidyuyuan <davidyuan@ebi.ac.uk>
2022-03-11 11:07:19 +0000

    Update pipeline versions to V2.

---
+++ gls/svc_nftw.init.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+PROJECT=${1:-'prj-int-dev-covid19-nf-gls'}
+
+# Service account key for NF Tower
+export SERVICE_ACCOUNT_NAME=nextflow-service-account
+export SERVICE_ACCOUNT_ADDRESS=${SERVICE_ACCOUNT_NAME}@${PROJECT}.iam.gserviceaccount.com
+export SERVICE_ACCOUNT_KEY=${SERVICE_ACCOUNT_NAME}-private-key.json
+
+gcloud iam service-accounts keys create --project "${PROJECT}" \
+  --iam-account="${SERVICE_ACCOUNT_ADDRESS}" \
+  --key-file-type=json ${SERVICE_ACCOUNT_KEY}
+export SERVICE_ACCOUNT_KEY_FILE=${DIR}/${SERVICE_ACCOUNT_KEY}
+export GOOGLE_APPLICATION_CREDENTIALS=${DIR}/${SERVICE_ACCOUNT_KEY}
