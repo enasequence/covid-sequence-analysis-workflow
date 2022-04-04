e8874aaede8bcf47cf07c9ae8edd5e86afd3b33f
davidyuyuan <davidyuan@ebi.ac.uk>
2022-04-04 23:43:56 +0100

    Update default values for update.receipt.sh.

--- update.receipt.sh
+++ update.receipt.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 batch_index=${1:-'0'}
-snapshot_date=${2:-'2022-03-22'}
+snapshot_date=${2:-'2022-04-12'}
 pipeline=${3:-'nanopore'}
 profile=${4:-'codon'}   # gls
-root_dir=${5:-'/nfs/production/cochrane/ena/users/davidyuan/covid-sequence-analysis-workflow'}   # gs://prj-int-dev-covid19-nf-gls
+root_dir=${5:-'/hps/nobackup/cochrane/ena/users/davidyuan/nextflow'}   # gs://prj-int-dev-covid19-nf-gls /nfs/production/cochrane/ena/users/davidyuan/covid-sequence-analysis-workflow
 dataset_name=${6:-'sarscov2_metadata'}
 project_id=${7:-'prj-int-dev-covid19-nf-gls'}
 
