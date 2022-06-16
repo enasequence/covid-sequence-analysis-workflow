ea82e59bf4334d124ef11e73fcca8c0edf0e7157
davidyuyuan <davidyuan@ebi.ac.uk>
2022-06-16 18:38:24 +0100

    Adding submitted_bytes to search query.

--- bq/view_submission_metadata.sql
+++ bq/view_submission_metadata.sql
@@ -1,4 +1,29 @@
 CREATE VIEW prj-int-dev-covid19-nf-gls.sarscov2_metadata.submission_metadata
+AS SELECT
+  DISTINCT T2.run_accession AS run_id,
+  T2.instrument_platform AS platform,
+  T2.instrument_model AS model,
+  T2.first_public,
+  T2.first_created,
+  T2.country,
+  T2.collection_date,
+  T1.time_submitted,
+  T1.snapshot_date,
+  -- IFNULL(T1.snapshot_date, '2022-06-27') AS snapshot_date,
+  T3.analysis_accession,
+  T3.sample_accession,
+  T3.submitted_bytes,
+  T3.analysis_date
+FROM (`prj-int-dev-covid19-nf-gls.sarscov2_metadata.submission_receipts` T1
+  FULL JOIN
+    `prj-int-dev-covid19-nf-gls.sarscov2_metadata.analysis_archived` T3
+  ON
+    REGEXP_EXTRACT(T1.file_submitted, r'^([A-Z0-9]+)_[a-z]+.[a-z]+.gz$') = T3.run_ref)
+INNER JOIN
+  `prj-int-dev-covid19-nf-gls.sarscov2_metadata.sra_index` T2
+ON
+  T2.run_accession = T3.run_ref
+
   -- AS SELECT
   --   DISTINCT T2.run_accession AS run_id,
   --   T2.instrument_platform AS platform,
@@ -14,26 +39,3 @@ CREATE VIEW prj-int-dev-covid19-nf-gls.sarscov2_metadata.submission_metadata
   --   `prj-int-dev-covid19-nf-gls.sarscov2_metadata.sra_index` T2
   -- WHERE
   --   T2.run_accession = REGEXP_EXTRACT(T1.file_submitted, r'^([A-Z0-9]+)_[a-z]+.[a-z]+.gz$')
-AS SELECT
-  DISTINCT T2.run_accession AS run_id,
-  T2.instrument_platform AS platform,
-  T2.instrument_model AS model,
-  T2.first_public,
-  T2.first_created,
-  T2.country,
-  T2.collection_date,
-  T1.time_submitted,
-  T1.snapshot_date,
-  -- IFNULL(T1.snapshot_date, '2022-06-27') AS snapshot_date,
-  T3.analysis_accession,
-  T3.sample_accession,
-  T3.analysis_date
-FROM (`prj-int-dev-covid19-nf-gls.sarscov2_metadata.submission_receipts` T1
-  FULL JOIN
-    `prj-int-dev-covid19-nf-gls.sarscov2_metadata.analysis_archived` T3
-  ON
-    REGEXP_EXTRACT(T1.file_submitted, r'^([A-Z0-9]+)_[a-z]+.[a-z]+.gz$') = T3.run_ref)
-INNER JOIN
-  `prj-int-dev-covid19-nf-gls.sarscov2_metadata.sra_index` T2
-ON
-  T2.run_accession = T3.run_ref
--- set.archived.sh
+++ set.archived.sh
@@ -6,7 +6,7 @@ project_id=${2:-'prj-int-dev-covid19-nf-gls'}
 # DIR where the current script resides
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-output_dir="${DIR}/results/"; mkdir -p "${output_dir}"
+output_dir="${DIR}/results"; mkdir -p "${output_dir}"
 
 ###########################################################
 # Get all analyses archived under parent_study = PRJEB45555
@@ -23,12 +23,12 @@ echo "** Updating ${dataset_name}.analysis_archived table. **"
 ## PRJEB45619 consensus_fasta_gz
 #curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'result=analysis&query=study_accession%3D%22PRJEB45619%22&fields=sample_accession%2Crun_ref&format=tsv&limit=0' \
 #  "https://www.ebi.ac.uk/ena/portal/api/search" > ./archived_consensus_fasta.tsv
-curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'result=analysis&query=parent_study%3D%22PRJEB45555%22&fields=sample_accession%2Crun_ref%2Canalysis_date&format=tsv&limit=0' \
+curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'result=analysis&query=parent_study%3D%22PRJEB45555%22&fields=sample_accession%2Crun_ref%2Canalysis_date%2Csubmitted_bytes&format=tsv&limit=0' \
   "https://www.ebi.ac.uk/ena/portal/api/search" > "${output_dir}/analysis_archived.tsv"
 gsutil -m cp "${output_dir}/analysis_archived.tsv" "gs://${dataset_name}/analysis_archived.tsv" && \
   bq --project_id="${project_id}" load --source_format=CSV --replace=true --skip_leading_rows=1 --field_delimiter=tab \
   --autodetect "${dataset_name}.analysis_archived" "gs://${dataset_name}/analysis_archived.tsv" \
-  "analysis_accession:STRING,sample_accession:STRING,run_ref:STRING,analysis_date:DATE"
+  "analysis_accession:STRING,sample_accession:STRING,run_ref:STRING,analysis_date:DATE,submitted_bytes:NUMERIC"
 
 #################################
 # delete runs from sra_processing
