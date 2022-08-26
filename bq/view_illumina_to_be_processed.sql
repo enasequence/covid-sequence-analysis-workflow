7d81d69c850f60cb276f147a4135663a70e29900
davidyuyuan <davidyuan@ebi.ac.uk>
2022-08-26 13:32:43 +0100

    Bucketize analysis_date as snapshot_date

--- bq/view_illumina_to_be_processed.sql
+++ bq/view_illumina_to_be_processed.sql
@@ -20,7 +20,7 @@ WHERE
     `prj-int-dev-covid19-nf-gls.sarscov2_metadata.sra_processing` T3)
   AND T1.run_accession NOT IN (
   SELECT
-    T4.run_id
+    T4.run_accession
   FROM
     `prj-int-dev-covid19-nf-gls.sarscov2_metadata.submission_metadata` T4)
 ORDER BY
--- bq/view_submission_metadata.sql
+++ bq/view_submission_metadata.sql
@@ -1,42 +1,42 @@
 CREATE VIEW prj-int-dev-covid19-nf-gls.sarscov2_metadata.submission_metadata
 AS SELECT
-  DISTINCT T2.run_accession AS run_id,
+  T1.analysis_accession,
+  T1.sample_accession,
+  T1.submitted_bytes,
+  T1.analysis_date,
+  T2.run_accession,
   T2.instrument_platform AS platform,
   T2.instrument_model AS model,
   T2.first_public,
   T2.first_created,
   T2.country,
   T2.collection_date,
-  T1.time_submitted,
-  -- T1.snapshot_date,
   (CASE
-    WHEN T3.analysis_date IS NULL THEN NULL
-    WHEN T3.analysis_date < '2022-03-22' THEN '2022-03-22'
-    WHEN T3.analysis_date >= '2022-03-22' AND T3.analysis_date < '2022-04-12' THEN '2022-04-12'
-    WHEN T3.analysis_date >= '2022-04-12' AND T3.analysis_date < '2022-05-23' THEN '2022-05-23'
-    WHEN T3.analysis_date >= '2022-05-23' AND T3.analysis_date < '2022-06-27' THEN '2022-06-27'
-    WHEN T3.analysis_date >= '2022-06-27' AND T3.analysis_date < '2022-07-25' THEN '2022-07-25'
-    WHEN T3.analysis_date >= '2022-07-25' AND T3.analysis_date < '2022-08-22' THEN '2022-08-22'
-    WHEN T3.analysis_date >= '2022-08-22' AND T3.analysis_date < '2022-09-26' THEN '2022-09-26'
-    WHEN T3.analysis_date >= '2022-09-26' AND T3.analysis_date < '2022-10-24' THEN '2022-10-24'
-    WHEN T3.analysis_date >= '2022-10-24' AND T3.analysis_date < '2022-11-21' THEN '2022-11-21'
-    WHEN T3.analysis_date >= '2022-11-21' AND T3.analysis_date < '2022-12-19' THEN '2022-12-19'
+    WHEN T1.analysis_date IS NULL THEN NULL
+    WHEN T1.analysis_date < '2022-03-22' THEN '2022-03-22'
+    WHEN T1.analysis_date >= '2022-03-22' AND T1.analysis_date < '2022-04-12' THEN '2022-04-12'
+    WHEN T1.analysis_date >= '2022-04-12' AND T1.analysis_date < '2022-05-23' THEN '2022-05-23'
+    WHEN T1.analysis_date >= '2022-05-23' AND T1.analysis_date < '2022-06-27' THEN '2022-06-27'
+    WHEN T1.analysis_date >= '2022-06-27' AND T1.analysis_date < '2022-07-25' THEN '2022-07-25'
+    WHEN T1.analysis_date >= '2022-07-25' AND T1.analysis_date < '2022-08-22' THEN '2022-08-22'
+    WHEN T1.analysis_date >= '2022-08-22' AND T1.analysis_date < '2022-09-26' THEN '2022-09-26'
+    WHEN T1.analysis_date >= '2022-09-26' AND T1.analysis_date < '2022-10-24' THEN '2022-10-24'
+    WHEN T1.analysis_date >= '2022-10-24' AND T1.analysis_date < '2022-11-21' THEN '2022-11-21'
+    WHEN T1.analysis_date >= '2022-11-21' AND T1.analysis_date < '2022-12-19' THEN '2022-12-19'
     ELSE 'TBD'
   END
     ) AS snapshot_date,
-  T3.analysis_accession,
-  T3.sample_accession,
-  T3.submitted_bytes,
-  T3.analysis_date
-FROM ((`prj-int-dev-covid19-nf-gls.sarscov2_metadata.submission_receipts` T1
-  FULL JOIN
-    `prj-int-dev-covid19-nf-gls.sarscov2_metadata.analysis_archived` T3
-  ON
-    REGEXP_EXTRACT(T1.file_submitted, r'^([A-Z0-9]+)_[a-z]+.[a-z]+.gz$') = T3.run_ref)
-  INNER JOIN
-    `prj-int-dev-covid19-nf-gls.sarscov2_metadata.sra_index` T2
-  ON
-    T2.run_accession = T3.run_ref)
+  T3.time_submitted
+FROM
+  `prj-int-dev-covid19-nf-gls.sarscov2_metadata.analysis_archived` T1
+LEFT JOIN
+  `prj-int-dev-covid19-nf-gls.sarscov2_metadata.sra_index` T2
+ON
+  T2.run_accession = T1.run_ref
+LEFT JOIN
+  `prj-int-dev-covid19-nf-gls.sarscov2_metadata.submission_receipts` T3
+ON
+  T3.analysis_accession = T1.analysis_accession;
 
   -- AS SELECT
   --   DISTINCT T2.run_accession AS run_id,
