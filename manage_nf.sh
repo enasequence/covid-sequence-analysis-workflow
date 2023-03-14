04260a62b297e217ef637654cf7c19f0c2757eeb
Sandeep Selvakumar <sands@ebi.ac.uk>
2023-03-14 14:48:46 +0000

    Refactor run.nextflow.sh and modify start.gls.jobs.sh to work on GCP

---
+++ manage_nf.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+index=${1:-'1'}
+batch_size=${2:-'15000'}
+project_id=${3:-'prj-int-dev-covid19-nf-gls'}
+dataset_name=${4:-'sarscov2_metadata'}
+table_name=${5:-'nanopore_to_be_processed'}
+input_dir=${6:-'/data/'}
+pipeline=${7:-'nanopore'}
+profile=${8:-'gls'}
+root_dir=${9:-'gls'}
+snapshot_date=${10:-'2023/03/14'}
+test_submission=${11:-'true'}
+
+offset=$((index * batch_size))
+echo ""
+echo "** Retrieving and reserving batch ${index} with the size of ${batch_size} from the offset of ${offset}. **"
+
+sql="SELECT * FROM ${project_id}.${dataset_name}.${table_name} LIMIT ${batch_size} OFFSET ${offset}"
+bq --project_id="${project_id}" --format=csv query --use_legacy_sql=false --max_rows="${batch_size}" "${sql}" \
+	| awk 'BEGIN{ FS=","; OFS="\t" }{$1=$1; print $0 }' > "${input_dir}/${table_name}_${index}.tsv"
+gsutil -m cp "${input_dir}/${table_name}_${index}.tsv" "gs://${dataset_name}/${table_name}_${index}.tsv" && \
+    bq --project_id="${project_id}" load --source_format=CSV --replace=false --skip_leading_rows=1 --field_delimiter=tab \
+    --max_bad_records=0 "${dataset_name}.sra_processing" "gs://${dataset_name}/${table_name}_${index}.tsv"
+if [ "$profile" != "gls" ]; then    
+	bsub -n 2 -M 4096 -q production "${DIR}/run.nextflow.sh" "${input_dir}/${table_name}_${index}.tsv" \
+    "${pipeline}" "${profile}" "${root_dir}" "${index}" "${snapshot_date}" "${test_submission}"
+else
+	"${DIR}/run.nextflow.sh" "${input_dir}/${table_name}_${index}.tsv" "${pipeline}" "${profile}" "${root_dir}" \
+	"${index}" "${snapshot_date}" "${test_submission}"  &
+fi
--- start.gls.jobs.sh
+++ start.gls.jobs.sh
@@ -3,14 +3,15 @@
 # DIR where the current script resides
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-pipeline=${1:-'illumina'}
+pipeline=${1:-'nanopore'}
 profile=${2:-'gls'}
 root_dir=${3:-'gs://prj-int-dev-covid19-nf-gls'}
-snapshot_date=${4:-'2022-06-27'}
-concurrency=${5:-'120'}   # Maximum concurrency determined by the bottleneck - the submission server at present
-batch_size=${6:-'9000'}   # takes 12 hours if 2 jobs, 72 hours if 40 jobs
+snapshot_date=${4:-'2023-03-14'}
+concurrency=${5:-'5'}   # Maximum concurrency determined by the bottleneck - the submission server at present
+batch_size=${6:-'5'}   # takes 12 hours if 2 jobs, 72 hours if 40 jobs
 dataset_name=${7:-'sarscov2_metadata'}
 project_id=${8:-'prj-int-dev-covid19-nf-gls'}
+test_submission='true'
 
 # Row count and batches
 table_name="${pipeline}_to_be_processed"
@@ -20,17 +21,42 @@ row_count=$(bq --project_id="${project_id}" --format=csv query --use_legacy_sql=
 ############################################
 # as defined as queueSize in nextflow.config
 ############################################
-queue_size=4
-batches=$(( row_count / batch_size + 1 ))
-num_of_jobs=$(( concurrency / queue_size ))
+queue_size=100
+batches=$(( (row_count + batch_size - 1) / batch_size ))
 
+
+batches=$(( (row_count + batch_size - 1) / batch_size ))
+num_of_jobs=$(( (concurrency + queue_size - 1) / queue_size ))
+
+input_dir="${DIR}/data/${snapshot_date}"; mkdir -p "${input_dir}"
+
+echo "Pipeline: ${pipeline}"
+echo "batches: ${batches}"
+echo "num_of_jobs: ${num_of_jobs}"
+echo "input_dir: ${input_dir}"
+sleep 5
 #for(( i=0; i<batches; i+=num_of_jobs )); do
 #  for (( j=i; j<i+num_of_jobs&&j<batches; j++ )); do
+#sql="SELECT * FROM ${project_id}.${dataset_name}.${table_name} LIMIT ${batch_size} OFFSET ${offset}"
+
 for (( j=0; j<num_of_jobs&&j<batches; j++ )); do
 #  bsub -n 2 -M 8192 -q production
-  "${DIR}/run.nextflow.sh" "${pipeline}" "${profile}" "${root_dir}" "${j}" "${snapshot_date}" "${batch_size}" &
+  echo "root_dir: ${root_dir}"
+  echo "pwd 1: $PWD"
+  mkdir -p "${root_dir}/${snapshot_date}/${pipeline}_${j}"; cd "${root_dir}/${snapshot_date}/${pipeline}_${j}" || exit
+  echo "pwd 2: $PWD"
+  offset=$((j * batch_size))
+  "${DIR}/manage_nf.sh" "${j}" "${batch_size}" "${project_id}" "${dataset_name}" "${table_name}" \
+  "${input_dir}" "${pipeline}" "${profile}" "${root_dir}" "${snapshot_date}" "${test_submission}"
 done
-#done
+
+#Move set.archived.sh and update.receipt.sh outside the loop
+#Comment out update.receipt.sh
+#"${DIR}/update.receipt.sh" "${batch_index}" "${snapshot_date}" "${pipeline}" "${profile}" "${root_dir}" "${dataset_name}" "${project_id}"
+"${DIR}/set.archived.sh" "${dataset_name}" "${project_id}"
+
+sql="CREATE OR REPLACE TABLE ${dataset_name}.sra_processing AS SELECT DISTINCT * FROM ${dataset_name}.sra_processing"
+bq --project_id="${project_id}" --format=csv query --use_legacy_sql=false "${sql}"
 
 #max_mem avg_mem swap stat exit_code exec_cwd exec_host
 #bjobs -u all -d -o "jobid job_name user submit_time start_time finish_time run_time cpu_used slots min_req_proc max_req_proc nthreads delimiter='^'" > jobs.csv
