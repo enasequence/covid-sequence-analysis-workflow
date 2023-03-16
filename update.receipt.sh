18145762368be54c05f6a69bb05550a51e19290f
mingyanisa <mingignim@hotmail.com>
2023-03-16 12:05:57 +0000

    update s3 download receipt

--- update.receipt.sh
+++ update.receipt.sh
@@ -29,7 +29,7 @@ if [ "${profile}" = 'gls' ]; then
 #  gsutil -m cp "gs://prj-int-dev-covid19-nf-gls/prepro/results/**/*_submissions.txt" "${output_dir}"
   find "${output_dir}" -type f -name '*_submissions.txt' -exec cat {} + >> "${output_dir}/${snapshot_date}_${pipeline}_${batch_index}_receipts.tsv"
 elif [ "${profile}" = 'awsbatch' ]; then
-  aws s3 cp "${pipeline_dir}/publishDir/**/*_submissions.txt" "${output_dir}"
+  aws s3 cp "${pipeline_dir}/publishDir/" "${output_dir}" --recursive --exclude "*" --include "*_submissions.txt"
   find "${output_dir}" -type f -name '*_submissions.txt' -exec cat {} + >> "${output_dir}/${snapshot_date}_${pipeline}_${batch_index}_receipts.tsv"
 else
 #  find "${pipeline_dir}/publishDir/" -type f -name '*_submissions.txt' > "${output_dir}/${pipeline}_receipts_${batch_index}.txt"
@@ -37,7 +37,6 @@ else
   find "${pipeline_dir}/publishDir/" -type f -name '*_submissions.txt' -exec cat {} + >> \
     "${output_dir}/${snapshot_date}_${pipeline}_${batch_index}_receipts.tsv"
 fi
-
 ########################################################
 # upload receipts and update the rows with snapshot_date
 ########################################################
