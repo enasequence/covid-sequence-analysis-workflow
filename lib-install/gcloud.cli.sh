ad6ff6ae2c4d69896b21eb01c34bade0eb03e87b
mingyanisa <mingignim@hotmail.com>
2023-04-04 13:26:40 +0000

    modified gcloud installation

--- lib-install/gcloud.cli.sh
+++ lib-install/gcloud.cli.sh
@@ -1,13 +1,13 @@
 echo "Installing gcloud cli"
+export $(grep -v '^#' .env | xargs)
 curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-421.0.0-linux-x86_64.tar.gz
 tar -xf ./google-cloud-cli-421.0.0-linux-x86_64.tar.gz
-mv google-cloud-sdk /opt/google-cloud-sdk
-ln -s /opt/google-cloud-sdk/bin/* /bin/
-rm google-cloud-cli-422.0.0-linux-x86_64.tar.gz
-# mv ./google-cloud-sdk ~/
-# rm ./google-cloud-cli-421.0.0-linux-x86_64.tar.gz
-# $HOME/google-cloud-sdk/install.sh
-# $HOME/google-cloud-sdk/bin/gcloud init
-gcloud auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS
-gcloud config set project "prj-int-dev-covid19-nf-gls"
+# sudo mv google-cloud-sdk /opt/google-cloud-sdk
+# wait
+# sudo ln -s /opt/google-cloud-sdk/bin/* /bin/
+# rm google-cloud-cli-421.0.0-linux-x86_64.tar.gz
+mv ./google-cloud-sdk ~/
+rm ./google-cloud-cli-421.0.0-linux-x86_64.tar.gz
+$HOME/google-cloud-sdk/install.sh
+$HOME/google-cloud-sdk/bin/gcloud init
 echo "Installed gcloud cli"
--- run.nextflow.sh
+++ run.nextflow.sh
@@ -25,7 +25,8 @@ echo ""
 echo "** Processing samples with ${DIR}/${pipeline}/${pipeline}.nf. **"
 
 if [ "$profile" = "awsbatch" ]; then
-      ${DIR}/lib-install/gcloud.cli.sh
+      gcloud auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS
+      gcloud config set project "prj-int-dev-covid19-nf-gls"
       project_bucket="prj-int-dev-ait-eosc-aws-eval"
       aws s3 cp ${batch_input} ${DIR}/data/ # download sample index file from s3 to local dir
       aws s3 cp "s3://${project_bucket}/${dataset_name}/" "${DIR}/data/" --recursive --exclude "*/*"  # download projects_accounts and .fa files
