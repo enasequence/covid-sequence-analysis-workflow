c82e55c22ed593890ba4694c6ae0d18c0d306891
mingyanisa <mingignim@hotmail.com>
2023-04-04 12:13:30 +0000

    rename gcloud install

---
+++ lib-install/gcloud.cli.sh
@@ -0,0 +1,11 @@
+curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-421.0.0-linux-x86_64.tar.gz
+tar -xf ./google-cloud-cli-421.0.0-linux-x86_64.tar.gz
+sudo mv google-cloud-sdk /opt/google-cloud-sdk
+sudo ln -s /opt/google-cloud-sdk/bin/* /bin/
+rm google-cloud-cli-422.0.0-linux-x86_64.tar.gz
+# mv ./google-cloud-sdk ~/
+# rm ./google-cloud-cli-421.0.0-linux-x86_64.tar.gz
+# $HOME/google-cloud-sdk/install.sh
+# $HOME/google-cloud-sdk/bin/gcloud init
+gcloud auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS
+gcloud config set project "prj-int-dev-covid19-nf-gls"
--- lib-install/google.cli.sh
+++
@@ -1,6 +0,0 @@
-curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-421.0.0-linux-x86_64.tar.gz
-tar -xf ./google-cloud-cli-421.0.0-linux-x86_64.tar.gz
-mv ./google-cloud-sdk ~/
-rm ./google-cloud-cli-421.0.0-linux-x86_64.tar.gz
-$HOME/google-cloud-sdk/install.sh
-$HOME/google-cloud-sdk/bin/gcloud init
