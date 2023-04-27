a8a150dc0de1e5590a03918765041909ae011cbf
mingyanisa <mingignim@hotmail.com>
2023-04-27 13:24:10 +0000

    add one param

--- illumina/bin/processing.py
+++ illumina/bin/processing.py
@@ -67,6 +67,6 @@ if __name__ == '__main__':
     print(args)
     sentry_sdk.init(f"{os.environ['SENTRY_URL']}")
     try:
-        run_process(args.work_dir, args.run_accession, args.projects_accounts_csv, args.input_file_1, args.input_file_2, args.sars2_fasta, args.study_accession)
+        run_process(args.work_dir, args.run_accession, args.projects_accounts_csv, args.input_file_1, args.input_file_2, args.sars2_fasta, args.task_cpus, args.study_accession)
     except Exception as e:
         sentry_sdk.capture_exception(e)
