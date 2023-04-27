84dfb2ab30a7bdcd8624cc182d9bf39d8130208d
mingyanisa <mingignim@hotmail.com>
2023-04-27 16:34:36 +0000

    remove workdir

--- illumina/bin/processing.py
+++ illumina/bin/processing.py
@@ -7,12 +7,6 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Start processing script')
 
-parser.add_argument('-w',
-                    '--work_dir',
-                    help="working directory",
-                    type=str,
-                    required=True)
-
 parser.add_argument('-r',
                     '--run_accession',
                     help="run_accession",
@@ -54,9 +48,9 @@ parser.add_argument('-s',
 
 args = parser.parse_args()
 
-def run_process(work_dir, run_accession, projects_accounts_csv, input_file_1, input_file_2, sars2_fasta, task_cpus, study_accession:str='PRJEB45555'):
+def run_process(run_accession, projects_accounts_csv, input_file_1, input_file_2, sars2_fasta, task_cpus, study_accession:str='PRJEB45555'):
     # Define the Bash command to run
-    bash_command=f"bash {work_dir} {run_accession} {projects_accounts_csv} {input_file_1} {input_file_2} {sars2_fasta} {task_cpus} {study_accession}"
+    bash_command=f"bash map_to_ref.sh {run_accession} {projects_accounts_csv} {input_file_1} {input_file_2} {sars2_fasta} {task_cpus} {study_accession}"
     # Run the Bash command and capture the output
     output = subprocess.check_output(bash_command, shell=True)
     # Print the output
@@ -71,7 +65,7 @@ if __name__ == '__main__':
                     ],
                     attach_stacktrace=True,)
     try:
-        run_process(args.work_dir, args.run_accession, args.projects_accounts_csv, args.input_file_1, args.input_file_2, args.sars2_fasta, args.task_cpus, args.study_accession)
+        run_process(args.run_accession, args.projects_accounts_csv, args.input_file_1, args.input_file_2, args.sars2_fasta, args.task_cpus, args.study_accession)
     except Exception as e:
         sentry_sdk.capture_exception(e)
     finally:
