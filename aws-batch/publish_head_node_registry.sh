6da06bf27b18550842a915b9e0f44a8e8fde75d0
mingyanisa <mingignim@hotmail.com>
2023-06-08 08:40:31 +0000

    remove error condition

--- aws-batch/publish_head_node_registry.sh
+++ aws-batch/publish_head_node_registry.sh
@@ -4,9 +4,10 @@
 # The repository (Branch aws-batch) will be cloned in the docker image.
 ###
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source ../.env
 # sudo yum install jq -y
 ### Option 1: Create ECR for head node
-# aws ecr get-login-password | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com
+aws ecr get-login-password --region=${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com
 # aws ecr create-repository --tags Key=covid-pipeline,Value=true  --repository-name nextflow-head
 # export REPO_URI="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/nextflow-head"
 
@@ -15,8 +16,8 @@ export REPO_URI="quay.io/enasequence/ena-sars-cov2-aws-batch-head"
 export IMG_TAG="test" # $(date +%F)
 echo $REPO_URI $DIR
 # Log in to your Quay.io account
+# docker rmi ${REPO_URI}:${IMG_TAG}
 echo "$QUAY_PASSWORD" | docker login quay.io --username $QUAY_USERNAME --password-stdin
-docker rmi ${REPO_URI}:${IMG_TAG}
 docker build -t ${REPO_URI}:${IMG_TAG} ${DIR}
 echo "IMAGE NAME = ${REPO_URI}:${IMG_TAG}"
 docker tag ${REPO_URI}:${IMG_TAG} ${REPO_URI}:${IMG_TAG}
