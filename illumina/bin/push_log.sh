253cf647931fc9a609073314bd87c31188083f20
mingyanisa <mingignim@hotmail.com>
2023-06-13 16:12:05 +0000

    update push log

--- illumina/bin/push_log.sh
+++ illumina/bin/push_log.sh
@@ -1,11 +1,25 @@
 #!/usr/bin/env bash
 run_accession=${1}
 err_file=${2}
+profile=${3}
 if grep -qi "error" ${err_file};  then
-    ## Send the log data to Datadog using the API
     err_log_content=$(jq -Rs '.' ${err_file})
-    log_msg=$(jq -n --arg error_status "ERROR" \
-            --arg ts "$(date '+%Y-%m-%d %H:%M:%S')" \
-            --arg error_msg "${err_log_content}" \
-            '{severity:$error_status, message:$error_msg, times:$ts}' )
+    log_msg=$(cat <<-END
+        {
+            "severity": "ERROR", 
+            "message": ${err_log_content}, 
+            "logging.googleapis.com/labels":{ 
+                "run_accession" : "$run_accession", 
+                "compute_resource" : "$profile"
+            }
+        }
+END
+    )
+    gcloud logging write covid_pipeline_logs "${log_msg}" --project="prj-int-dev-covid19-nf-gls" --payload-type=json --severity=ERROR
+    # ## Send the log data to Datadog using the API
+    # err_log_content=$(jq -Rs '.' ${err_file})
+    # log_msg=$(jq -n --arg error_status "ERROR" \
+    #         --arg ts "$(date '+%Y-%m-%d %H:%M:%S')" \
+    #         --arg error_msg "${err_log_content}" \
+    #         '{severity:$error_status, message:$error_msg, times:$ts}' )
 fi
