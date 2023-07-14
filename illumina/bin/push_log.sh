200d37c667f6ab718f52318f90b6e03c6b63ca95
mingyanisa <mingignim@hotmail.com>
2023-07-14 09:35:19 +0000

    add message limit

--- illumina/bin/push_log.sh
+++ illumina/bin/push_log.sh
@@ -3,12 +3,13 @@ run_accession=${1}
 err_file=${2}
 profile=${3}
 gcloud_project_id=${4}
+CHAR_LIMIT=2000
 if grep -qi "error" ${err_file};  then
     err_log_content=$(jq -Rs '.' ${err_file})
     log_msg=$(cat <<-END
         {
             "severity": "ERROR", 
-            "message": ${err_log_content}, 
+            "message": ${err_log_content:0:CHAR_LIMIT}, 
             "logging.googleapis.com/labels":{ 
                 "run_accession" : "$run_accession", 
                 "compute_resource" : "$profile"
